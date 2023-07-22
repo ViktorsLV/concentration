e8cab923bf0db486d19fe74055e799f0908e49f2
ViktorsLV <viktors.ch11@gmail.com>
2023-07-22 13:53:02 +0300

    card game logic implemented

--- src/app/components/GameBoard.jsx
+++ src/app/components/GameBoard.jsx
@@ -5,22 +5,48 @@ import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 const gameCards = [
-  { src: '/images/bunny.webp' },
-  { src: '/images/coyote.webp' },
-  { src: '/images/daffy.webp' },
-  { src: '/images/lola.webp' },
-  { src: '/images/runner.webp' },
-  { src: '/images/taz.webp' },
-  { src: '/images/tweety.webp' },
-  { src: '/images/sylwester.webp' }
+  { src: '/images/bunny.webp', matched: false },
+  { src: '/images/coyote.webp', matched: false },
+  { src: '/images/daffy.webp', matched: false },
+  { src: '/images/lola.webp', matched: false },
+  { src: '/images/runner.webp', matched: false },
+  { src: '/images/taz.webp', matched: false },
+  { src: '/images/tweety.webp', matched: false },
+  { src: '/images/sylwester.webp', matched: false }
 ];
 
 const GameBoard = () => {
   const [cards, setCards] = useState();
+  const [turnCount, setTurnCount] = useState(0);
+
+  const [firstCard, setFirstCard] = useState();
+  const [secondCard, setSecondCard] = useState();
 
   useEffect(() => {
-    randomizeCards();
+    resetGame();
   }, []);
+
+  useEffect(() => {
+    if (firstCard && secondCard) {
+      if (firstCard.src === secondCard.src) {
+        console.log('match');
+        setCards((prev) => prev.map((card) => (card.id === firstCard.id || card.id === secondCard.id ? { ...card, matched: true } : card)));
+        resetValues();
+      } else {
+        console.log('no match');
+        setTimeout(() => {
+          resetValues();
+        }, 1000);
+      }
+    }
+  }, [firstCard, secondCard]);
+
+  useEffect(() => {
+    if (cards?.every((card) => card.matched)) {
+      alert(`You won in ${turnCount} turns!`);
+      resetGame();
+    }
+  }, [cards]);
 
   const randomizeCards = () => {
     const randomCards = [...gameCards, ...gameCards].sort(() => Math.random() - 0.5).map((card, index) => ({ ...card, id: index }));
@@ -29,22 +55,64 @@ const GameBoard = () => {
     return randomCards;
   };
 
+  const handleCardClick = (card) => {
+    console.log(card);
+    firstCard ? setSecondCard(card) : setFirstCard(card);
+  };
+
+  const resetValues = () => {
+    setTurnCount((prev) => prev + 1);
+    setFirstCard();
+    setSecondCard();
+  };
+
+  const resetGame = () => {
+    setFirstCard();
+    setSecondCard();
+    randomizeCards();
+    setTurnCount(0);
+  };
+
   return (
-    <div className="grid w-full grid-flow-row grid-cols-4 grid-rows-4 gap-2 mx-auto sm:gap-6">
-      {/* <button onClick={randomizeCards}>randomize</button> */}
-      {cards?.map((card) => (
-        <Card key={card.id} src={card.src} />
-      ))}
-    </div>
+    <>
+      <div className="grid w-full grid-flow-row grid-cols-4 grid-rows-4 gap-2 mx-auto sm:gap-6">
+        {cards?.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            firstCard={firstCard}
+            secondCard={secondCard}
+            setTurnCount={setTurnCount}
+            turnCount={turnCount}
+            handleClick={() => handleCardClick(card)}
+            turnedAround={firstCard === card || secondCard === card || card.matched}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center w-full mt-8">
+        <p className="text-2xl font-medium text-white">Turns: {turnCount}</p>
+      </div>
+    </>
   );
 };
 
 export default GameBoard;
 
-const Card = ({ src }) => {
+const Card = ({ card, handleClick, turnedAround }) => {
   return (
-    <div className="w-full h-[100px] relative bg-gray-300 rounded-xl hover:cursor-pointer hover:opacity-90 duration-300 transition-all ease-in-out">
-      <Image src={src} alt="card" fill={true} objectFit="contain" />
+    <div className="w-full h-[100px] relative bg-gray-300 rounded-xl">
+      {turnedAround ? (
+        <Image src={card.src} alt="card front" fill={true} objectFit="contain" />
+      ) : (
+        <Image
+          onClick={handleClick}
+          src="/images/looneyTunes.avif"
+          alt="card back"
+          fill={true}
+          objectFit="cover"
+          className="transition-all duration-300 ease-in-out rounded-xl opacity-85 hover:cursor-pointer hover:opacity-90"
+        />
+      )}
     </div>
   );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <main className="relative h-screen py-24 overflow-y-scroll bg-gray-900 min-h-max isolate sm:py-32">
+        <main className="relative h-screen py-24 overflow-x-hidden overflow-y-scroll bg-gray-900 min-h-max isolate sm:py-32">
           <div className="relative px-6 isolate pt-14 lg:px-8">
             <BackgroundDark />
             {children}
