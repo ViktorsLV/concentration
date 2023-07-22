a7682675cdf1276d2e823775c5f18a4d6522ff76
ViktorsLV <viktors.ch11@gmail.com>
2023-07-22 13:53:13 +0300

    title change

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -4,7 +4,8 @@ const Game = () => {
   return (
     <div className="mx-auto sm:px-6 max-w-7xl">
       <div className="w-full mx-auto sm:max-w-7xl">
-        <h2 className="mb-6 text-4xl font-bold tracking-tight text-center text-white sm:text-6xl">Concentration Game</h2>
+        <h2 className="mb-2 text-4xl font-bold tracking-tight text-center text-white sm:text-6xl">Concentration Game</h2>
+        <p className="mt-2 mb-4 text-sm font-bold text-center text-white">(Looney Tunes Edition)</p>
         <div className="mx-auto ">
           <GameBoard />
         </div>
