fde142499067769c5a3d0b2a761ac305afdbda3b
ViktorsLV <viktors.ch11@gmail.com>
2023-07-22 15:06:38 +0300

    responsive sizing corrections

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -3,10 +3,10 @@ import GameBoard from '../components/GameBoard';
 const Game = () => {
   return (
     <div className="mx-auto sm:px-6 max-w-7xl">
-      <div className="w-full mx-auto sm:max-w-7xl">
-        <h2 className="mb-2 text-4xl font-bold tracking-tight text-center text-white sm:text-6xl">Concentration Game</h2>
+      <div className="w-full mx-auto sm:max-w-2xl">
+        <h2 className="mb-2 text-3xl font-bold tracking-tight text-center text-white sm:text-6xl">Concentration Game</h2>
         <p className="mt-2 mb-4 text-sm font-bold text-center text-white">(Looney Tunes Edition)</p>
-        <div className="mx-auto ">
+        <div className="mx-auto">
           <GameBoard />
         </div>
       </div>
