3757e10b7fda03a14233daf7c001e4802bc7e73d
ViktorsLV <viktors.ch11@gmail.com>
2023-07-22 14:54:45 +0300

    set click disabled while cards turn around

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,9 +1,14 @@
-const Button = ({ text, onClick }) => {
+const Button = ({ text, onClick, disabled }) => {
   return (
     <>
       <button
         type="button"
-        className="rounded-full bg-indigo-600 w-full px-5 sm:px-10 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        disabled={disabled}
+        className={`${
+          disabled
+            ? 'bg-gray-400 text-slate-200'
+            : 'text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+        } rounded-full  w-full px-5 sm:px-10 py-2.5 text-sm font-semibold shadow-sm `}
         onClick={onClick}>
         {' '}
         {text}
--- src/app/components/GameBoard.jsx
+++ src/app/components/GameBoard.jsx
@@ -21,8 +21,9 @@ const GameBoard = () => {
   const [cards, setCards] = useState();
   const [turnCount, setTurnCount] = useState(0);
 
-  const [firstCard, setFirstCard] = useState();
-  const [secondCard, setSecondCard] = useState();
+  const [firstCard, setFirstCard] = useState(null);
+  const [secondCard, setSecondCard] = useState(null);
+  const [clickDisabled, setClickDisabled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
@@ -31,14 +32,19 @@ const GameBoard = () => {
 
   useEffect(() => {
     if (firstCard && secondCard) {
+      setClickDisabled(true);
       if (firstCard.src === secondCard.src) {
         console.log('match');
         setCards((prev) => prev.map((card) => (card.id === firstCard.id || card.id === secondCard.id ? { ...card, matched: true } : card)));
         resetValues();
+        setTimeout(() => {
+          setClickDisabled(false);
+        }, 300);
       } else {
         console.log('no match');
         setTimeout(() => {
           resetValues();
+          setClickDisabled(false);
         }, 1000);
       }
     }
@@ -50,13 +56,6 @@ const GameBoard = () => {
     }
   }, [cards]);
 
-  const randomizeCards = () => {
-    const randomCards = [...gameCards, ...gameCards].sort(() => Math.random() - 0.5).map((card, index) => ({ ...card, id: index }));
-    console.log(randomCards);
-    setCards(randomCards);
-    return randomCards;
-  };
-
   const handleCardClick = (card) => {
     console.log(card);
     firstCard ? setSecondCard(card) : setFirstCard(card);
@@ -66,6 +65,13 @@ const GameBoard = () => {
     setTurnCount((prev) => prev + 1);
     setFirstCard();
     setSecondCard();
+  };
+
+  const randomizeCards = () => {
+    const randomCards = [...gameCards, ...gameCards].sort(() => Math.random() - 0.5).map((card, index) => ({ ...card, id: index }));
+    console.log(randomCards);
+    setCards(randomCards);
+    return randomCards;
   };
 
   const resetGame = () => {
@@ -83,14 +89,19 @@ const GameBoard = () => {
     <>
       <div className="grid w-full grid-flow-row grid-cols-4 grid-rows-4 gap-2 mx-auto sm:gap-6">
         {cards?.map((card) => (
-          <Card key={card.id} card={card} handleClick={() => handleCardClick(card)} turnedAround={firstCard === card || secondCard === card || card.matched} />
+          <Card
+            key={card.id}
+            card={card}
+            handleClick={clickDisabled ? null : () => handleCardClick(card)}
+            turnedAround={firstCard === card || secondCard === card || card.matched}
+          />
         ))}
       </div>
       <div className="flex justify-center w-full mt-8">
         <p className="text-2xl font-medium text-white">Turns: {turnCount}</p>
       </div>
       <div className="flex items-center w-4/5 mx-auto mt-8 sm:w-2/5 lg:w-1/5">
-        <Button text="Restart Game" onClick={resetGame} />
+        <Button text="Restart Game" onClick={resetGame} disabled={turnCount === 0 && !firstCard} />
       </div>
       {isOpen && <Alert toggleAlert={toggleAlert} open={isOpen} turnCount={turnCount} />}
     </>
